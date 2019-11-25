411b0dbbce6cf0cfb3fa7abe0259134a06229015
jiyeon <i3048i@naver.com>
2019-11-26 01:08:56 +0900

    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# hikingmobileapp
-2019 등산 커뮤니티 어플
+## 2019 Capstone Design Project
+- 멤버 : 이지연, 윤준기, 김범철
+- 제작 기간 : Sep 2019 ~ Dec 2019
+
+## Demo
+![capsone](/img/capstone.gif)
+
+
+## What We Used
+- 등산로 정보 : 공공 데이터 포털 산정보 RESTful API
+- 이미지 정보 : BING 이미지 검색
+- 지도 : Google Map Platform
+
+
+## Sample Screens
+
+|screens||
+|:---:|:----:|
+| ![img1](/img/capstone1.png) </br>main page | ![img2](/img/capstone_map.png) </br> google map |
+| ![img3](/img/capstone_search.png) </br> search page | ![img4](/img/capstone_detail.png) </br> detail page |
+
+
+## To Do List
+- [x] 이미지 검색 불러오기
+- [ ] 구글맵 등산로 정보 가져오기
+- [ ] GeoJSON??? 
--- hiking_front/ios/Runner/AppDelegate.m
+++ hiking_front/ios/Runner/AppDelegate.m
@@ -1,18 +1,23 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 
-@import Firebase;
+// Add the following import.
+#import "GoogleMaps/GoogleMaps.h"
+//#import Firebase;
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  [FIRApp configure];
 
+  //[FIRApp configure];
+
+  // Add the following line, with your API key
+  [GMSServices provideAPIKey: @"AIzaSyDqM78Plt99H1ErpNH9hxBtuZRxOBvoo5g"];
   // Override point for customization after application launch.
-  //return [super application:application didFinishLaunchingWithOptions:launchOptions];
-  return YES;
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+
 }
 
 @end
